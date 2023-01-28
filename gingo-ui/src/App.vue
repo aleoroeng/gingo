a340cb34ddec92a4a35c326be1f7e7e4625721b3
unknown <oro99eng@gmail.com>
2023-01-27 23:51:05 -0500

    add ui for vuejs

---
+++ gingo-ui/src/App.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>HI from app component</h1>
+</template>
--- main.go
+++ main.go
@@ -1,12 +1,44 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v3"
 )
 
+type Config struct {
+	Datasource struct {
+		Url string `yaml:"url"`
+	} `yaml:"datasource"`
+}
+
+func readConfig() {
+	config, err := os.Open("./config/config.yml")
+
+	if err != nil {
+		log.Println(err)
+	}
+	defer config.Close()
+	var appConfig *Config
+	decoder := yaml.NewDecoder(config)
+	errors := decoder.Decode(&appConfig)
+
+	if errors != nil {
+		log.Println(errors)
+	}
+	fmt.Println("url: ", appConfig.Datasource.Url)
+	fmt.Println("Hi")
+}
+
 func main() {
+	readConfig()
 	r := gin.Default()
+
 	r.GET("/index", func(c *gin.Context) {
+
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
